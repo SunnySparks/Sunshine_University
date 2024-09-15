9f3202e87f0c28a8a6764a20b6df0534c877edb2
José Francisco Silva Díaz <Josediazt6@outlook.com>
2024-09-14 19:22:01 -0600

    Migrate Exito and footer components

--- Sunshine_University/src/App.jsx
+++ Sunshine_University/src/App.jsx
@@ -2,6 +2,8 @@ import "./styles/global.scss";
 import About from "./Components/About/About";
 import Hero from "./Components/Hero/Hero";
 import Oferta from "./Components/OfertaAcademica/Oferta";
+import Exito from "./Components/ExitoAcademico/Exito";
+import Footer from "./Components/Layout/Footer";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Hero />
       <About />
       <Oferta />
+      <Exito />
+      <Footer />
     </>
   );
 }
