7ad217ebfc78f0a9e5e733c45a8c08c838698dee
José Francisco Silva Díaz <Josediazt6@outlook.com>
2025-04-08 23:30:51 -0600

    Deploy latest build

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/Sunshine_University/",
+  build: {
+    outDir: "dist", // Ensure the build is outputting to 'dist' folder
+  },
 });
