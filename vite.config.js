0186f179efe2ef32e494be9aca200f38f6b6fe7e
José Francisco Silva Díaz <Josediazt6@outlook.com>
2025-04-10 22:07:36 -0600

    configure package json and vite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const isGitHubPages = process.env.BUILD_TARGET === "gh-pages";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Sunshine_University/",
+  base: isGitHubPages ? "/Sunshine_University/" : "/",
+  plugins: [react()],
   build: {
     outDir: "dist", // Ensure the build is outputting to 'dist' folder
   },
